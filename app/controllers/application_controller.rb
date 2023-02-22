e437b84507724863aa0da625ec2d5f0dea904aa2
kbonga2-appdev <stuwrkr-kbonga2@mx.uillinois.edu>
2023-02-22 18:08:29 +0000

    rock first part

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,10 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+  def play_rock
+    # redirect_to("https://www.google.com")
+    # render({ :html => "<h1> Hello </h1>".html_safe})
 
+    render({ :template => "xtemplates/user_rock.html.erb" })
+  end
 end
