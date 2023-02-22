e1447b38ab098aa0a5edb747cd9e9f7ba09b70cb
kbonga2-appdev <stuwrkr-kbonga2@mx.uillinois.edu>
2023-02-22 22:02:34 +0000

    ppaper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,37 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper.html.erb")
 
   # Add your actions below this line
   # ================================
+
+  def homepage
+    render({ :template => "xtemplates/rules.erb"})
+  end
+
+
   def play_rock
     # redirect_to("https://www.google.com")
     # render({ :html => "<h1> Hello </h1>".html_safe})
 
-    render({ :template => "xtemplates/user_rock.html.erb" })
+    render({ :template => "xtemplates/user_rock.html.erb"  })
   end
+
+  def play_paper
+
+    @comp_move = ["rock", "paper", "scissors"].sample 
+
+    
+    
+     if @comp_move == "rock" 
+     @outcome = "won"
+     elsif @comp_move == "paper" 
+     @outcome = "tied"
+     elsif @comp_move == "scissors" 
+     @outcome = "lost"
+     end 
+    
+
+   render({ :template => "xtemplates/user_paper.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+get("/", {:controller => "application",:action => "Homepage"})
 get("/rock", {:controller => "application",:action => "play_rock"})
+get("/paper", {:controller => "application",:action => "play_paper"})
+
 end
